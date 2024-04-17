4db8272fdd348aae6980247f81cfc2034143d6d8
Akash <ag308669@gmail.com>
2024-04-17 16:00:17 +0530

    modified loading screen

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -2,8 +2,9 @@ import { Loader2 } from "lucide-react";
 
 const LoadingPage = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <Loader2 className="animate-spin" />
+    <div className="pt-28 flex flex-col items-center justify-center gap-4">
+      <Loader2 className="animate-spin size-6" />
+      <p className="animate-pulse text-xl font-bold">Loading...</p>
     </div>
   );
 };
