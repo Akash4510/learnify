b1b5cb48f56cd11b6f122de57a13d15d6fede9a6
Akash Gupta <ag308669@gmail.com>
2024-11-11 21:21:26 +0530

    added image in about section

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,24 +1,32 @@
+import Image from "next/image";
+
 const AboutPage = () => {
   return (
-    <div className="p-4 space-y-4">
-      <h1 className="text-4xl">About LearnUPIND</h1>
-
-      <div>
-        <p className="text-lg">
-          Its started on [__DATE__] Since then we are inspiring youngsters or
-          those people who willing to learn the valuable and modish courses.
-        </p>
-        <p className="text-lg">
-          We are trying to bring learning to people instead of people to
-          learning. Hence, we are trying to modify their entrepreneurship
-          nature.
-        </p>
-        <p className="text-lg">
-          Here we are with a lot of courses designed in a way that will help
-          people to learn a lot of things and take things to a new level.
-        </p>
+    <>
+      <div className="w-full h-64 relative">
+        <Image src="/about.png" alt="about" fill className="object-cover" />
       </div>
-    </div>
+
+      <div className="p-4 py-6 mb-10 space-y-4">
+        <h1 className="text-3xl md:text-4xl">About LearnUPIND</h1>
+
+        <div className="text-primary-foreground/80 space-y-1.5">
+          <p>
+            Its started on [__DATE__] Since then we are inspiring youngsters or
+            those people who willing to learn the valuable and modish courses.
+          </p>
+          <p>
+            We are trying to bring learning to people instead of people to
+            learning. Hence, we are trying to modify their entrepreneurship
+            nature.
+          </p>
+          <p>
+            Here we are with a lot of courses designed in a way that will help
+            people to learn a lot of things and take things to a new level.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
