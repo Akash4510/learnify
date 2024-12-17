25295e8f70d4eadc294d2dfb9b847fba9a2e926b
Akash Gupta <ag308669@gmail.com>
2024-12-17 10:12:44 +0530

    Modified privacy policy and t&c

--- app/(main)/privacy-policy/page.tsx
+++ app/(main)/privacy-policy/page.tsx
@@ -12,88 +12,146 @@ const PrivacyPolicyPage = () => {
         subtitleClassName="text-base"
       />
 
-      <div className="space-y-3 leading-7">
-        <p className="list-item list-inside">
-          PRIVACY POLICY At LearnUPIND, accessible from{" "}
-          <Link
-            href="/privacy-policy"
-            className="text-blue-400 underline underline-offset-2"
-          >
-            https://learnupind.com/privacy-policy
-          </Link>
-        </p>
+      <div className="space-y-7 leading-7">
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Who we are</h3>
+          <p>
+            Our website address is:{" "}
+            <Link
+              href="/"
+              className="text-blue-400 underline underline-offset-2"
+            >
+              https://learnupind.com
+            </Link>
+          </p>
+        </div>
 
-        <p className="list-item list-inside">
-          One of our main priorities is the privacy of our visitors. This
-          Privacy Policy document contains types of information that is
-          collected and recorded by LearnUPIND and how we use it. If you have
-          additional questions or require more information about our Privacy
-          Policy, do not hesitate to contact us. This Privacy Policy applies
-          only to our online activities and is valid for visitors to our website
-          with regards to the information that they shared and/or collect in
-          LearnUPIND. This policy is not applicable to any information collected
-          offline or via channels other than this website. Consent By using our
-          website, you hereby consent to our Privacy Policy and agree to its
-          terms. Information we collect The personal information that you are
-          asked to provide, and the reasons why you are asked to provide it,
-          will be made clear to you at the point we ask you to provide your
-          personal information. If you contact us directly, we may receive
-          additional information about you such as your name, email address,
-          phone number, the contents of the message and/or attachments you may
-          send us, and any other information you may choose to provide.
-        </p>
-        <p className="list-item list-inside">
-          When you register for an account, we may ask for your contact
-          information, including items such as name, company name, address,
-          email address, and telephone number. How we use your information? We
-          use the information we collect in various ways, including to: Provide,
-          operate, and maintain our website Improve, personalize, and expand our
-          website Understand and analyze how you use our website develop new
-          products, services, features, and functionality Communicate with you,
-          either directly or through one of our partners, including for customer
-          service, to provide you with updates and other information relating to
-          the website, and for marketing and promotional purposes, and to send
-          you emails.
-        </p>
-        <p className="list-item list-inside">
-          To find and prevent fraud log files LearnUPIND follows a standard
-          procedure of using log files. These files log visitors when they visit
-          websites. All hosting companies do this and a part of hosting
-          services&apos; analytics. The information collected by log files
-          include internet protocol (IP) addresses, browser type, Internet
-          Service Provider (ISP), date and time stamp, referring/exit pages, and
-          possibly the number of clicks. These are not linked to any information
-          that is personally identifiable. The purpose of the information is for
-          analyzing trends, administering the site, tracking users&apos;
-          movement on the website, and gathering demographic information.
-        </p>
-        <p className="list-item list-inside">
-          Cookies and Web Beacons Like any other website, LearnUPIND uses
-          “cookies”. These cookies are used to store information including
-          visitors&apos; preferences, and the pages on the website that the
-          visitor accessed or visited. The information is used to optimize the
-          users&apos; experience by customizing our web page content based on
-          visitors&apos; browser type and/or other information.
-        </p>
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Comments</h3>
+          <p>
+            When visitors leave comments on the site we collect the data shown
+            in the comments form, and also the visitor&apos;s IP address and
+            browser user agent string to help spam detection.
+          </p>
+          <p>
+            An anonymized string created from your email address (also called a
+            hash) may be provided to the Gravatar service to see if you are
+            using it. The Gravatar service privacy policy is available here:{" "}
+            <Link
+              href="/"
+              className="text-blue-400 underline underline-offset-2"
+            >
+              https://www.learnupind.com
+            </Link>
+            . After approval of your comment, your profile picture is visible to
+            the public in the context of your comment.
+          </p>
+        </div>
 
-        <p className="list-item list-inside">
-          Advertising Partners Privacy Policies you may consult this list to
-          find the Privacy Policy for each of the advertising partners of
-          LearnUPIND Third-party ad servers or ad networks uses technologies
-          like cookies, JavaScript, or Web Beacons that are used in their
-          respective advertisements and links that appear on LearnUPIND, which
-          are sent directly to users&apos; browser. They automatically receive
-          your IP address when this occurs. These technologies are used to
-          measure the effectiveness of their advertising campaigns and/or to
-          personalize the advertising content that you see on websites that you
-          visit. Note that LearnUPIND has no access to or control over these
-          cookies that are used by third-party advertisers. Third Party Privacy
-          Policies LearnUPIND&apos;s Privacy Policy does not apply to other
-          advertisers or websites. Thus, we are advising you to consult the
-          respective Privacy Policies of these third-party ad servers for more
-          detailed information. It may include their practices and instructions
-          about how to opt-out of certain options.
-        </p>
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Media</h3>
+          <p>
+            If you upload images to the website, you should avoid uploading
+            images with embedded location data (EXIF GPS) included. Visitors to
+            the website can download and extract any location data from images
+            on the website.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Cookies</h3>
+          <p className="list-item list-inside">
+            If you leave a comment on our site you may opt-in to saving your
+            name, email address and website in cookies. These are for your
+            convenience so that you do not have to fill in your details again
+            when you leave another comment. These cookies will last for one
+            year.
+          </p>
+          <p className="list-item list-inside">
+            If you visit our login page, we will set a temporary cookie to
+            determine if your browser accepts cookies. This cookie contains no
+            personal data and is discarded when you close your browser.
+          </p>
+          <p className="list-item list-inside">
+            When you log in, we will also set up several cookies to save your
+            login information and your screen display choices. Login cookies
+            last for two days, and screen options cookies last for a year. If
+            you select “Remember Me”, your login will persist for two weeks. If
+            you log out of your account, the login cookies will be removed.
+          </p>
+          <p className="list-item list-inside">
+            If you edit or publish an article, an additional cookie will be
+            saved in your browser. This cookie includes no personal data and
+            simply indicates the post ID of the article you just edited. It
+            expires after 1 day.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">
+            Embedded content from other websites
+          </h3>
+          <p className="list-item list-inside">
+            Articles on this site may include embedded content (e.g. videos,
+            images, articles, etc.). Embedded content from other websites
+            behaves in the exact same way as if the visitor has visited the
+            other website.
+          </p>
+          <p className="list-item list-inside">
+            These websites may collect data about you, use cookies, embed
+            additional third-party tracking, and monitor your interaction with
+            that embedded content, including tracking your interaction with the
+            embedded content if you have an account and are logged in to that
+            website.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Who we share your data with?</h3>
+          <p>
+            If you request a password reset, your IP address will be included in
+            the reset email.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">How long we retain your data?</h3>
+          <p className="list-item list-inside">
+            If you leave a comment, the comment and its metadata are retained
+            indefinitely. This is so we can recognize and approve any follow-up
+            comments automatically instead of holding them in a moderation
+            queue.
+          </p>
+          <p className="list-item list-inside">
+            For users that register on our website (if any), we also store the
+            personal information they provide in their user profile. All users
+            can see, edit, or delete their personal information at any time
+            (except they cannot change their username). Website administrators
+            can also see and edit that information.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">
+            What rights you have over your data?
+          </h3>
+          <p>
+            If you have an account on this site, or have left comments, you can
+            request to receive an exported file of the personal data we hold
+            about you, including any data you have provided to us. You can also
+            request that we erase any personal data we hold about you. This does
+            not include any data we are obliged to keep for administrative,
+            legal, or security purposes.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h3 className="text-xl font-bold">Where we send your data?</h3>
+          <p>
+            Visitor comments may be checked through an automated spam detection
+            service.
+          </p>
+        </div>
       </div>
     </div>
   );
