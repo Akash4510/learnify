ccdeb24cc20c3d1b5b5d85d7be56b69b37cc0c99
Akash Gupta <ag308669@gmail.com>
2025-01-27 00:29:21 +0530

    Update email sender address and add logging for verification and password reset emails

--- lib/mail.ts
+++ lib/mail.ts
@@ -10,21 +10,27 @@ const domain = process.env.NEXT_PUBLIC_APP_URL!;
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/verify-email?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const res = await resend.emails.send({
+    from: "learnupind@gmail.com",
     to: email,
     subject: "Confirm your email",
     react: VerifyEmailTemplate({ confirmLink }),
   });
+
+  console.log(`SendVerificationEmailRes: ${res}`);
+  return res;
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const res = await resend.emails.send({
+    from: "learnupind@gmail.com",
     to: email,
     subject: "Reset your password",
     react: ResetPasswordTemplate({ resetLink }),
   });
+
+  console.log(`SendPasswordResetEmailRes: ${res}`);
+  return res;
 };
